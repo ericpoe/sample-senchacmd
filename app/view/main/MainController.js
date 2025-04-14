6941b66cc59b637d4afb2090ac27f5665669260f
Eric Poe <eric.poe@gmail.com>
2025-04-13 23:48:49 -0500

    Test ES11 and ES14 features

--- app/view/main/MainController.js
+++ app/view/main/MainController.js
@@ -11,9 +11,29 @@ Ext.define('Sample.senchacmd.view.main.MainController', {
         Ext.Msg.confirm('Confirm', 'Are you sure?', 'onConfirm', this);
     },
 
+
+
     onConfirm: function (choice) {
+        let testing1 = {
+            name: 'John Doe',
+            age: 30,
+            city: 'New York'
+        };
+        let testing2 = [
+            'Strawberry',
+            'Eggplant',
+            'Nectarine',
+            'Cherry',
+            'Huckleberry',
+            'Apple',
+        ];
         if (choice === 'yes') {
-            //
+            Ext.Msg.alert('Testing ES11 Optional Chaining & Null Coalescing', 'You chose: ' + testing1?.city ?? 'City not found');
+            console.log(testing1?.city ?? 'City not found');
+        }
+        if (choice === 'no') {
+            Ext.Msg.alert('Testing ES14 Array toSorted', 'You chose: ' + testing2.toSorted());
+            console.log(testing2.toSorted());
         }
     }
 });
